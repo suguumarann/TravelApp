48eb7fe6a14c17c6ea07ad98809639afd956f74c
Sugumaran Thiagarajan <118104398+suguumarann@users.noreply.github.com>
2024-03-01 19:48:41 +0800

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Travel",
-  description: "Travel UI/UX App",
+  title: "Travel"
 };
 
 export default function RootLayout({
